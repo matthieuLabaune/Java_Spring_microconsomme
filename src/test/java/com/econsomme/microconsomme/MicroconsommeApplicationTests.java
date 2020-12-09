6da34961a28412f43c696c536a1e41e863489300
Matthieu <matthieu.labaune@le-campus-numerique.fr>
2020-12-09 09:54:11 +0100

    [ADD] · First Example with SpringBoot RestTemplate (get quote)

---
+++ src/test/java/com/econsomme/microconsomme/MicroconsommeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econsomme.microconsomme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroconsommeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
