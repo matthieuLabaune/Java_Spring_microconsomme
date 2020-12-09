ae3dfc7a68d713df19e7eded95e9656362fb8eab
Matthieu <matthieu.labaune@le-campus-numerique.fr>
2020-12-09 11:05:49 +0100

    [UPD] · Complete CRUD RestTemplate

--- src/main/java/com/econsomme/microconsomme/MicroconsommeApplication.java
+++ src/main/java/com/econsomme/microconsomme/MicroconsommeApplication.java
@@ -1,12 +1,7 @@
 package com.econsomme.microconsomme;
 
-import com.econsomme.microconsomme.model.Quote;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,23 +9,12 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class MicroconsommeApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(MicroconsommeApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicroconsommeApplication.class, args);
 	}
 
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Quote quote = restTemplate.getForObject(
-					"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-			log.info(quote.toString());
-		};
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
---
+++ src/main/java/com/econsomme/microconsomme/controller/CarController.java
@@ -0,0 +1,65 @@
+package com.econsomme.microconsomme.controller;
+
+import com.econsomme.microconsomme.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+
+@RestController
+public class CarController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    //GET ALL CARS
+    @RequestMapping(value = "/cars")
+    public String getProductList() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        return restTemplate.exchange(
+                "http://localhost:8080/cars", HttpMethod.GET, entity, String.class).getBody();
+    }
+
+    //POST A NEW CAR
+    @RequestMapping(value = "/cars/store", method = RequestMethod.POST)
+    public String createCars(@RequestBody Car car) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Car> entity = new HttpEntity<Car>(car,headers);
+
+        return restTemplate.exchange(
+                "http://localhost:8080/cars/store", HttpMethod.POST, entity, String.class).getBody();
+    }
+
+    // UPDATE A CAR
+    @RequestMapping(value = "/cars/{id}", method = RequestMethod.PUT)
+    public String updateCars(@PathVariable("id") String id, @RequestBody Car car) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Car> entity = new HttpEntity<Car>(car,headers);
+
+        return restTemplate.exchange(
+                "http://localhost:8080/cars/"+id, HttpMethod.PUT, entity, String.class).getBody();
+    }
+
+    //DELETE A CAR
+    @RequestMapping(value = "/cars/{id}", method = RequestMethod.DELETE)
+    public String deleteCar(@PathVariable("id") String id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Car> entity = new HttpEntity<Car>(headers);
+
+        return restTemplate.exchange(
+                "http://localhost:8080/cars/"+id, HttpMethod.DELETE, entity, String.class).getBody();
+    }
+}
